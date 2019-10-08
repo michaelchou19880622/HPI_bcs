9a70a4f336a69b938e916949fecfe27bef4f39f6
michaelchou-XtremeApp <mic_chou@hpicorp.com.tw>
2019-10-08 11:15:02 +0800

    [ 修正 ] 修改security忽略/api/userStatusUpdate URL驗證

--- src/main/java/com/hpicorp/bcs/config/SecurityConfig.java
+++ src/main/java/com/hpicorp/bcs/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -82,5 +83,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.headers().cacheControl().disable();
         
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/userStatusUpdate/**");
     }
 }
